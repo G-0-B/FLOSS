bca00bf83d7929f7aabca650fd24c2e73e25c545
kalisam <37503662+kalisam@users.noreply.github.com>
2026-07-15 02:57:37 -0400

    feat: seal and restore-test salvage capsules
    (cherry picked from commit 6c1edee486f30195f6852445c73f6a3b709a6543)

--- packages/salvage_spine/__init__.py
+++ packages/salvage_spine/__init__.py
@@ -10,16 +10,24 @@ from .models import (
     ResultStatus,
     canonical_json_bytes,
 )
+from .restore import PlaneRestoreResult, VerificationRecord, restore_and_verify
+from .seal import CapsuleVerificationError, seal_capsule, verify_checksums
 
 __all__ = [
     "CapsuleRecord",
+    "CapsuleVerificationError",
     "PlaneEligibility",
     "PlaneId",
     "PlaneRecord",
+    "PlaneRestoreResult",
     "PlaneSensitivity",
     "PlaneVerification",
     "ResultStatus",
+    "VerificationRecord",
     "canonical_json_bytes",
+    "restore_and_verify",
+    "seal_capsule",
+    "verify_checksums",
 ]
 
 __version__ = "0.1.0"
